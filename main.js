9cfef1a0d13bd2fe6fb3237f1887ef472f51ed31
J9pro <janine239@gmail.com>
2023-01-03 17:12:48 +0100

    pokemon

--- main.js
+++ main.js
@@ -1,35 +1,64 @@
-const btn = document.getElementById('btn').addEventListener("click", getFetch)
+const btn = document.getElementById('btn').addEventListener("click", getRandom)
 
-function getFetch() {
-    // const choice = document.querySelector('input').value
-    // console.log(choice)
-    const url = ` https://pokeapi.co/api/v2/pokemon/`
+let pokemon = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot',
+    'rattata', 'raticate']
 
-    fetch(url)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-            console.log(data.results)
-            // data.results.forEach(obj => {
-            for (const key in data.results) {
-                console.log(typeof key.name)
-                // this.name = array[Math.floor(Math.random() * array.length - 1)]
-                //const random = (Math.random().toString() * obj.name) + 1;
-
-                //console.log(obj.name)
-
-            };
-
-
-
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
+function getRandom() {
+    let random = pokemon[Math.floor(Math.random() * pokemon.length)]
+    console.log(random)
+    const li = document.createElement('li')
+    li.textContent = random
+    document.querySelector('ul').appendChild(li)
 }
 
 
 
 
-// const li = document.createElement('li')
-                //li.textContent = obj.name
-                //document.querySelector('ul').appendChild(li)
+//const btn = document.getElementById('btn').addEventListener("click", getFetch)
+
+// function getFetch() {
+//     // const choice = document.querySelector('input').value
+//     // console.log(choice)
+//     const url = `https://pokeapi.co/api/v2/pokemon/`
+
+//     fetch(url)
+//         .then(res => res.json()) // parse response as JSON
+//
+//         .then(data => {
+//             //console.log(data.results)
+//             for (let { name } of data.results) {
+//                 const list = name;
+//                 //console.log(list)
+//                 const random = Math.floor(Math.random() * list.length)
+//                 // console.log(random)
+
+
+//                 // let arr = list.split(' ,').join(list, '')
+//                 // console.log(arr)
+//             }
+
+
+
+
+//
+
+
+//             //console.log(obj.name)
+
+//             // });
+
+
+
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`)
+//         });
+// }
+
+
+
+
+
+                   //data.results.forEach(obj => {
+            //for (const key in data.results) {
+            //const names = obj.name
